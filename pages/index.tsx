29d6980f24f97abf0b5b9703f1fe8ce82125af77
Sobhan Bera <sobhanbera258@gmail.com>
2022-04-11 03:59:03 +0530

    rest of the updates

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/header'
+import Footer from '../components/footer'
+import MainContent from '../components/main'
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+
+            <MainContent />
+
+            <Footer />
+        </div>
+    )
+}
